cfe3a05e52413629d5e08b219d8411aa1fbcbee4
Ozzyboshi <gun101@email.it>
2018-09-21 11:31:29 +0200

    preparing aminet upload

--- src/airstrike.c
+++ src/airstrike.c
@@ -64,12 +64,12 @@ static int general_setup(void)
   assert(sprite_global.display);
   SDL_ShowCursor(SDL_DISABLE);
 
-  if (cfgnum("sound",0) == 1)
-  {
+  //if (cfgnum("sound",0) == 1)
+  //{
       sound_setup();
       /* place the ear on the ground, in the center */
       sound_move_ear(screen_w/2,screen_h);
-  }
+  //}
   sprite_setup();
   text_setup();
   console_setup();
